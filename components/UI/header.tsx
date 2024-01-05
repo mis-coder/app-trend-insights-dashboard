61f4fa3bbc3217434b3159a407cd23ac8454b8cb
Antra Verma <antraverma99@gmail.com>
2024-01-05 15:19:08 +0530

    added menu icon

--- components/UI/header.tsx
+++ components/UI/header.tsx
@@ -1,6 +1,9 @@
+import { Menu } from "lucide-react";
+
 const Header = () => {
   return (
     <div className="bg-red h-16 w-screen flex items-center justify-start text-xl font-bold text-white uppercase pl-4">
+      <Menu className="mr-6" size="28"/>
       App Trendz
     </div>
   );
