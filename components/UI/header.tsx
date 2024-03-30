cd2a498b1afff3da18137f60704a3986d14d8aa3
Antra Verma <antraverma99@gmail.com>
2024-03-30 13:40:12 +0530

    added header hid sidebar

---
+++ components/UI/header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="bg-red h-16 w-screen flex items-center justify-start text-xl font-bold text-white uppercase p-5">
+      <p className="text-white">App Trendz</p>
+    </div>
+  );
+};
+
+export default Header;
